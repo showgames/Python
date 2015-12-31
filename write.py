3e5f90f908e0d7f1e4ba6c7e54803fc040f6894c
Shohei Hayashi <2329shohei@gmail.com>
2015-12-31 22:58:00 +0900

    first commit

--- write.py
+++ write.py
@@ -1,6 +1,11 @@
-name = 'hoge hoge'
+amino_acids = ["His", "Arg", "Asp", "Asn"]
+
+sentence = " "
+for x in amino_acids:
+	sentence += x
+	sentence += ":"
 
 fout = open('namefile', 'wt')
-fout.write(name)
+fout.write(sentence)
 fout.close()
 
